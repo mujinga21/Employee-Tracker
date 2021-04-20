17e81abeb92884d1e44c1214a2d34507f3e6e9e8
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-04-19 23:54:41 -0400

    added view codes

---
+++ Solution/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(30)
+);
+
+CREATE TABLE employee_role (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL,
+   department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+ 
+);
+CREATE TABLE employee (
+ id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ first_name VARCHAR(30),
+ last_name VARCHAR(30),
+ role_id INT,
+ FOREIGN KEY (role_id) REFERENCES employee_role(id)
+ manager_id INT
+ FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
