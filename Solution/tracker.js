cefc40ab45519d9ec379b6cc7718d6c4bb526bb6
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-04-20 23:33:13 -0400

    added search

--- Solution/tracker.js
+++ Solution/tracker.js
@@ -34,132 +34,80 @@ connection.connect((err) => {
           'Update employee manager',
         ],
       })
-      .then((answer) => {
-        switch (answer.action) {
-          case 'Add departments':
-            artistSearch();
-            break;
   
-          case 'Find all artists who appear more than once':
-            multiSearch();
-            break;
-  
-          case 'Find data within a specific range':
-            rangeSearch();
-            break;
-  
-          case 'Search for a specific song':
-            songSearch();
-            break;
-  
-          case 'Exit':
-            connection.end();
-            break;
-  
-          default:
-            console.log(`Invalid action: ${answer.action}`);
-            break;
-        }
-      });
-  };
-  
-  const artistSearch = () => {
-    inquirer
-      .prompt({
-        name: 'artist',
-        type: 'input',
-        message: 'What artist would you like to search for?',
-      })
-      .then((answer) => {
-        const query = 'SELECT position, song, year FROM top5000 WHERE ?';
-        connection.query(query, { artist: answer.artist }, (err, res) => {
-          if (err) throw err;
-          res.forEach(({ position, song, year }) => {
-            console.log(
-              `Position: ${position} || Song: ${song} || Year: ${year}`
-            );
-          });
-          runSearch();
-        });
-      });
-  };
-  
-  const multiSearch = () => {
-    const query =
-      'SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1';
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-      res.forEach(({ artist }) => console.log(artist));
-      runSearch();
+    .then((answer) => {
+      switch (answer.action) {
+    
+
+        case 'view all employees':
+          employeeSearch();
+          break;
+
+
+
+
+        case 'view employees by manager':
+          rangeSearch();
+          break;
+
+        case 'Add employee':
+          songSearch();
+          break;
+
+        case 'Find artists with a top song and top album in the same year':
+          songAndAlbumSearch();
+          break;
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+          connection.end();
+      }
     });
   };
   
-  const rangeSearch = () => {
-    inquirer
-      .prompt([
-        {
-          name: 'start',
-          type: 'input',
-          message: 'Enter starting position: ',
-          validate(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          },
-        },
-        {
-          name: 'end',
-          type: 'input',
-          message: 'Enter ending position: ',
-          validate(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          },
-        },
-      ])
-      .then((answer) => {
-        const query =
-          'SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?';
-        connection.query(query, [answer.start, answer.end], (err, res) => {
+      const employeeSearch = () => {
+        connection.query("SELECT * FROM employee", (err, res) => {
           if (err) throw err;
-          res.forEach(({ position, song, artist, year }) =>
-            console.log(
-              `Position: ${position} || Song: ${song} || Artist: ${artist} || Year: ${year}`
-            )
-          );
-          runSearch();
+      
+          // Log all results of the SELECT statement
+          console.log(res);
+          // connection.end();
         });
-      });
-  };
-  
-  const songSearch = () => {
-    inquirer
-      .prompt({
-        name: 'song',
-        type: 'input',
-        message: 'What song would you like to look for?',
-      })
-      .then((answer) => {
-        console.log(`You searched for "${answer.song}"`);
-        connection.query(
-          'SELECT * FROM top5000 WHERE ?',
-          { song: answer.song },
-          (err, res) => {
-            if (err) throw err;
-            if (res[0]) {
-              console.log(
-                `Position: ${res[0].position} || Song: ${res[0].song} || Artist: ${res[0].artist} || Year: ${res[0].year}`
-              );
-              runSearch();
-            } else {
-              console.error('Song not found :(\n');
-              runSearch();
-          }
-           
-        ;
-      });
-  };
-  
+    }
+    addEmployee: (req, res) => {
+      if (err) throw err;
+    }
+    let message = '';
+    let first_name =
+    
+
+// const employeeByManagerSearch = () => {
+//   inquirer
+//     .prompt({
+//       name: 'employee',
+//       type: 'input',
+//       message: 'What manager employee would you like to view?',
+//     })
+//     .then((answer) => {
+//       let query =
+//      'SELECT employee.role_id, employee.first_name, employee.last_name, role.salary, role.title ';
+//       query +=
+//         'FROM employee INNER JOIN role ON (employee.id and role.id ';
+//       query +=
+//         '= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position';
+
+//       connection.query(query, [answer.artist, answer.artist], (err, res) => {
+//         console.log(`${res.length} matches found!`);
+//         res.forEach(({ year, position, artist, song, album }, i) => {
+//           const num = i + 1;
+//           console.log(
+//             `${num} Year: ${year} Position: ${position} || Artist: ${artist} || Song: ${song} || Album: ${album}`
+//           );
+//         });
+
+//         runSearch();
+//       });
+//     });
+// };
+
