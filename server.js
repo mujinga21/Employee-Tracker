005b84e7561096093ebaf87d2874d04e6d1ff933
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-05-23 09:03:27 -0400

    managers and roles

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const inquirer = require("inquirer");
 
 
 
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
+  multipleStatements: true,
   host: "localhost",
 
   // Your port; if not 3306
@@ -18,23 +19,11 @@ const connection = mysql.createConnection({
   password: "1234",
   database: "employee_tracker",
 });
-connection.connect((err) => {
+connection.connect(function(err) {
   if (err) throw err;
   runSearch();
 });
 
-// const queryAllEmployee = () => {
-//   connection.query("SELECT * FROM employee", (err, res) => {
-//     if (err) throw err;
-//     res.forEach(({ id, first_name, last_name, role_id, manager_id }) => {
-//       console.log(
-//         `${id} | ${first_name} | ${last_name} | ${role_id} | ${manager_id}`
-//       );
-//     });
-//     console.log("-----------------------------------");
-//     runSearch();
-//   });
-// };
 
 function runSearch  () {
   inquirer
@@ -44,123 +33,73 @@ function runSearch  () {
       message: "What would you like to do?",
       choices: [
         "view all employees",
-        "View all employees by manager",
+        "View all role",
+        "View all department",
         "Add employee",
-        "Remove employee",
-        "Update employee role",
-        "Update employee manager",
+        "Add role",
+        "Add department",
+        "Update role",
         "Exit",
       ]
     })
     .then(function(answer) {
       if(answer.action === 'view all employees'){
         viewEmployee();
-      }else if (answer.action === 'view all employee by manager'){
-        viewAllEmployeeByManager();
-      }else if (answer.action === 'add  employee'){
+      }else if (answer.action === 'view all role'){
+        viewRole();
+      }else if (answer.action === 'view all department'){
+        viewDepartment();
+      }else if (answer.action === 'add employee'){
         addEmployee();
-      }else if (answer.action === 'remove an employee'){
-        RemoveEmployee();
-      }else if (answer.action === 'Update employee role'){
-      updateEmployee();
-      }else if (answer.action === 'update employee manager'){
-      updateEmployee();
+      }else if (answer.action === 'add role'){
+        addRole();
+      }else if (answer.action === 'add department'){
+      addDepartment();
+      }else if (answer.action === 'update role'){
+      updateRole();
       }else if(answer.action === 'exit'){
         connection.end();
       }
       
     })
   }
-//     .then((answer) => {
-//       console.log('answer', answer);
-//       switch (answer.action) {
-//         case "view all employees":
-//            viewAllEmployee();
-//            break;
-      
-//         case "View all employees by manager":
-//           viewEmployeeByManager();
-//           break;
-        
-//         case "Add employee":
-//            addEmployee();
-//            break;
-          
-//         case "Remove employee":
-//            removeEmployee();
-//            break;
-        
-//         case "Update employee role":
-//            updateEmployeeRole();
-//            break;
-        
-//         case "Update employee manager":
-//            updateEmployeeManager();
-//            break;
-          
-//         case "exit":
-//           connection.end();
-//           break;
-//       }
-//     });
-// };
 
 
 function viewEmployee  () {
   var query= "SELECT * FROM employee";    
 connection.query(query, function (err, res) {
-  console.log(`Employee:`)
+  console.log(`EMPLOYEE:`)
   res.forEach(employee => {
-    console.log(`ID: $(employee.id'| Name: ${employee.first_name} ${employee.Last_name}  | Role ID: ${employee.role_id} | Manager ID: ${employee.manager_id}`);
+    console.log(`ID: ${employee.id} | Name: ${employee.first_name} ${employee.Last_name}  | Role ID: ${employee.role_id} | Manager ID: ${employee.manager_id}`);
   
   })
   runSearch();
 });
 };
 
+function viewRole () {
+  var query= "SELECT * FROM role";
+  connection.query(query, function (err, res) {
+    console.log(`ROLE:`)
+    res.forEach(role => {
+      console.log(`ID: ${role.id}| Title: ${role.title} | Salary: ${role.salary} | Department ID : ${role.department_id}`);
+
+    })
+    runSearch();
+  });
+};
 
 
-
-
-
-
-
-// const queryEmployeeByManager = () => {
-//   inquirer .prompt({
-//       name: 'manager_name',
-//       type: 'list',
-//       message: 'employee's manager:'
-//     })
-//     .then((answer) => {
-//       let query =
-//         'SELECT CONCAT(manager.first_name,'', manager.last_name)
-//       query +=
-//         'FROM  INNER JOIN role ON(role_id = employee.role_id && employee:manager_id = 'NULL');
-//       query +=
-//         '= department(department.id= role.department_id) WHERE (t;
-
-//       connection.query(query, [answer.artist, answer.artist], (err, res) => {
-//         console.log(`${res.length} matches found!`);
-//         res.forEach(({ year, position, artist, song, album }, i) => {
-//           const num = i + 1;
-//           console.log(
-//             `${num} Year: ${year} Position: ${position} || Artist: ${artist} || Song: ${song} || Album: ${album}`
-//           );
-//         });
-
-//         runSearch();
-//       });
-//     });
-// };
-
-
-
-
-// const addEmployee = () => {
-//   console.log("Inserting a new employee...\n");
-
-// };
-
+function viewDepartment () {
+  var query= "SELECT * FROM department";
+  connection.query(query, function (err, res) {
+    console.log(`DEPARTMENT:`)
+    res.forEach(department => {
+      console.log(`ID: ${department.id}| Name: ${department.name}`)
+    })
+    runSearch();
+  });
+};
 
 
 
